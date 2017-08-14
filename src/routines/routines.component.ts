3ecd1c751cb74e2e605464d3083014b54854a432
Aaron Marisi <aarongithub@outlook.com>
2017-08-13 21:12:29 -0400

    replace app component with routines component

---
+++ src/routines/routines.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-routines',
+  template: '<h1>Routines Component</h1>'
+})
+export class RoutinesComponent {}
--- src/routines/routines.module.ts
+++ src/routines/routines.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { RoutinesComponent } from './routines.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    RoutinesComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RoutinesComponent]
 })
-export class AppModule { }
+export class RoutinesModule { }
